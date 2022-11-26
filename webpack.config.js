b9b53702d1d3839cf33555e53af6bc5e15562734
KirllBashorin <99118225+KirllBashorin@users.noreply.github.com>
2022-11-26 15:32:02 +0700

    Подключил вебак

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: '/source/js/main.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'build/js'),
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "../css/vendors/vendors.css",
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+};
